e2115e33ea604c2b3e71050ed42e7b3b0d8905c3
Jorge Sevilla <solopunks803@gmail.com>
2025-11-05 17:25:32 -0300

    Add anotações para validação

--- ejb-module/src/main/java/com/example/ejb/model/Beneficio.java
+++ ejb-module/src/main/java/com/example/ejb/model/Beneficio.java
@@ -1,6 +1,9 @@
 package com.example.ejb.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PositiveOrZero;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -20,12 +23,15 @@ public class Beneficio {
     @Column(name = "ID")
     private Long id;
 
+    @NotBlank(message = "O nome não pode estar em branco")
     @Column(name = "NOME")
     private String nome;
 
     @Column(name = "DESCRICAO")
     private String descricao;
 
+    @NotNull(message = "O valor não pode ser nulo")
+    @PositiveOrZero(message = "O valor deve ser positivo ou zero")
     @Column(name = "VALOR")
     private BigDecimal valor;
 
